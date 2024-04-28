e2a7a331b1fc05b5050c842afa39491cc671ba69
namiqmahammadov <130308957+namiqmahammadov@users.noreply.github.com>
2024-04-27 22:36:04 -0700

    errror aradan qaldirildi

--- app.js
+++ app.js
@@ -7,60 +7,62 @@ let toInput = document.querySelector(".to-input");
 let form = document.querySelector(".from-form");
 let fromP = document.querySelector(".from-p");
 let toP = document.querySelector(".to-p");
-let from = "RUB",
-  to = "USD";
-fromInput.value = 100;
+let from = "RUB";
+let to = "USD";
 
 eventListeners();
 checkLi();
+getData();
 
 function eventListeners() {
   ulFrom.addEventListener("click", fromValue);
   ulTo.addEventListener("click", toValue);
-  fromInput.addEventListener("keyup", getData);
+  fromInput.addEventListener("input", getData);
   ulTo.addEventListener("click", getData);
   ulFrom.addEventListener("click", getData);
-  document.addEventListener("DOMContentLoaded", getData);
 }
 
-function getData(e) {
-  let out;
-  e.preventDefault();
+function getData() {
   fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_nfozWjag1DZ7LnYeaA3YaIofxyWUfXN5qBa9Kr3v&currencies=${to}&base_currency=${from}`)
     .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       return response.json();
     })
-
     .then((data) => {
-      out = Object.values(data.data)[0];
-      fromP.innerText = `1 ${from} = ${out} ${to}`;
-      toP.innerText = `1 ${to} = ${1 / out} ${from}`;
-      calc(out);
+      if (data && data.data) {
+        let out = Object.values(data.data)[0];
+        fromP.innerText = `1 ${from} = ${out} ${to}`;
+        toP.innerText = `1 ${to} = ${(1 / out).toFixed(4)} ${from}`;
+        calc(out);
+      } else {
+        throw new Error('Data structure error: Unable to retrieve currency data');
+      }
     })
     .catch((err) => {
+      console.error("Error fetching data:", err);
     });
 }
 
 function fromValue(e) {
   if (e.target.className === "li-from") {
     from = e.target.innerText;
-    console.log(from);
     checkLi();
   }
-  e.preventDefault();
 }
 
 function toValue(e) {
   if (e.target.className === "li-to") {
     to = e.target.innerText;
-    console.log(to);
     checkLi();
   }
-  e.preventDefault();
 }
+
 function calc(out) {
-  toInput.value = (out * fromInput.value).toFixed(2);
+  toInput.value = (parseFloat(fromInput.value.replace(',', '')) * out).toFixed(2);
 }
+
 function checkLi() {
   ulFromLi.forEach((item) => {
     item.classList.remove("active");
